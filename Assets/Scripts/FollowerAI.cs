560638ff39b3596fc1f14789d41790b1b74e2122
William Baal <williamxp95@gmail.com>
2025-06-25 19:38:18 +0200

    update followerAI

--- Assets/Scripts/FollowerAI.cs
+++ Assets/Scripts/FollowerAI.cs
@@ -7,17 +7,23 @@ public class FollowerAI : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform playerTransform;
+    private Vector3 startingPosition;
+
+    [Header("Paramètres de l'IA")]
+    public float aggroRange = 15f;
 
     void Start()
     {
         // On récupère le composant NavMeshAgent attaché à cet ennemi
         agent = GetComponent<NavMeshAgent>();
+        startingPosition = transform.position;
 
         // On trouve le joueur dans la scène grâce à son tag "Player"
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         if (playerObject != null)
         {
             playerTransform = playerObject.transform;
+            
         }
     }
 
@@ -26,7 +32,22 @@ public class FollowerAI : MonoBehaviour
         // Si le joueur existe, on dit à l'agent de se diriger vers sa position
         if (playerTransform != null)
         {
-            agent.SetDestination(playerTransform.position);
+            float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+            if (distanceToPlayer <= aggroRange)
+            {
+                // Le joueur est assez proche, donc on met à jour la destination de l'ennemi.
+                agent.SetDestination(playerTransform.position);
+            }
+            else
+            {
+                float distanceToStart = Vector3.Distance(transform.position, startingPosition);
+                // Le joueur est trop loin. L'ennemi s'arrête de le suivre.
+                
+                if (distanceToStart >= 0.5f)
+                {
+                    agent.SetDestination(startingPosition);
+                }
+            }
         }
     }
 
