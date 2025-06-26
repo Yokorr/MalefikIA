4dd3f6a9b28181b47596c61a3256546fdb50f62f
DruideCalme <benjamin.chassier@gmail.com>
2025-06-26 10:38:54 +0200

    scene fin

---
+++ Assets/Scripts/LastChoice.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LastChoice : MonoBehaviour
+{
+    public Button next;
+    public Button end;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (next != null)
+        {
+            next.onClick.AddListener(nextGame);
+        }
+        if (end != null)
+        {
+            end.onClick.AddListener(endGame);
+        }
+    }
+
+    public void nextGame()
+    {
+        SceneManager.LoadScene("FinAlive");
+    }
+
+    public void endGame()
+    {
+        SceneManager.LoadScene("FinDead");
+    }
+}
